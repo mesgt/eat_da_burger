2f723725f3fe20d0eb13199b80d4a7b7a80e0868
Maria Sargent <maria.marshall.83@gmail.com>
2021-02-09 15:44:47 -0600

    Added basic structure, schema, seed files, orm and connection.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, consumed) VALUES ('Bacon Cheeseburger', 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ("The Burger's Priest", 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ('Chicken Onion Crispy', 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ('Double Cheeseburger', 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ('Mega Burger', 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ('Mega Salad Burger', 'false');
+INSERT INTO burgers (burger_name, consumed) VALUES ('Texas Burger', 'false');
